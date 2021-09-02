9a938ef2992653eee08b3d7a2aac8062b5ee4d5b
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2021-09-02 16:15:34 +0300

    start

--- graph/settings.py
+++ graph/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hyxzq0l(88fwg06(wd*6(u(f&wp08e1ix3becoe+ko#9!#(o=t'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-hyxzq0l(88fwg06(wd*6(u(f&wp08e1ix3becoe+ko#9!#(o=t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'logistics.apps.LogisticsConfig',
 ]
 
 MIDDLEWARE = [
